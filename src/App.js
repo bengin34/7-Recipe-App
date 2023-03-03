7b397ebd606e9bae50344d3425f4a85d6c5694c9
bengin34 <becaglar3434@gmail.com>
2023-03-03 19:16:25 +0300

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import {Router, Route} from "react-router-dom" 
+import Header from "./components/Header";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <></>;
+  return <>
+    <Login />
+
+
+  </>;
 };
 
 export default App;
---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const REcipeCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default REcipeCard
